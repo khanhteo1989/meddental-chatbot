de1fa6fb3bb5c517cf17eaf3a9debef56e76a23a
KhangLee <150659223+khanhteo1989@users.noreply.github.com>
2025-04-12 22:36:18 +0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import Chatbox from "./components/Chatbox";
+
+export default function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen text-white">
+      <Chatbox />
+    </div>
+  );
+}
